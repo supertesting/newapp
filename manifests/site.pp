ca44a204efcf66e295491992ec0b0a447858f9c9
ubuntu <ubuntu@user.(none)>
2013-10-05 04:31:05 -0500

    fix nodes for cassandra in manifest

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,6 +8,6 @@ node "clone3.mydomain.org"
 	include cassandra
 	class { 'cassandra':
   		cluster_name => 'YourCassandraCluster',
-  		seeds        => [ '192.0.2.5', '192.0.2.23', '192.0.2.42', ],
+  		seeds        => [ 'clone2' ],
 	}
 }
